df76e7a2212b1cb5311efefa76a0742c39432646
Aaron Girard <girardaaron89@gmail.com>
2025-09-15 17:38:21 -0400

    updated child classes and main

--- src/Library/User.java
+++ src/Library/User.java
@@ -1,6 +1,6 @@
 package Library;
 
-public class User {
+public abstract class User {
 
     protected String name;
     protected String email;
@@ -29,4 +29,5 @@ public class User {
         return phoneNumber;
     }
 
+    abstract public void menu();
 }
